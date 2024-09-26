0588ad2dd26f0379d9fd9de954fee4810e129edb
krulod <krulod@proton.me>
2024-09-26 18:26:12 +0300

    pletyvo: handle http errors

--- src/pletyvo.ts
+++ src/pletyvo.ts
@@ -24,16 +24,21 @@ export class Pletyvo {
 		return this.request<data>('get', endpoint, body, undefined)
 	}
 
-	request<data>(method: 'get' | 'post', endpoint: string, body: unknown, query: PletyvoQuery = {}) {
+	async request<data>(method: 'get' | 'post', endpoint: string, body: unknown, query: PletyvoQuery = {}) {
 		const headers = new Headers
 		const network = this.network()
 		network && headers.set('Network', network)
 
-		return fetch( new URL( endpoint, this.urlBase() + '?' + new URLSearchParams(query as any) ), {
+		const response = await fetch(new URL(endpoint, this.urlBase() + '?' + new URLSearchParams(query as any)), {
 			method,
 			body: body === undefined ? undefined : JSON.stringify(body),
 			headers,
-		} ).then( (response) => response.json() as data )
+		})
+
+		if (response.status !== 200) {
+			throw new Error(`${response.status} ${response.statusText}: ${await response.text()}`)
+		}
+		return response.json() as data
 	}
 
 	signer(): DappAuthSigner {
